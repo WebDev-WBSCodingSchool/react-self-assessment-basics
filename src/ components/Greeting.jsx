a11ef3368a12f2b53a150a606022a4dd33c8706e
Jorge Paul <jserranopaul@gmail.com>
2025-06-02 10:58:45 +0200

    Greeting

---
+++ src/ components/Greeting.jsx
@@ -0,0 +1,8 @@
+const Greeting = () => {
+  /* TODO: read `name` from props and render “Hello, <name>!” within a div */
+  /* TODO: if no name is provided, render "Hello, stranger!" */
+  /* TODO: if name is empty, render "Hello, stranger!" */
+  /* TODO: if name is NOT a string, render "Hello???" */
+};
+
+export default Greeting;
